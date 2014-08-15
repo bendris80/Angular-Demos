1fe75d36efd0de587ae18b6935f4234133799a0a
Roger <Roger@Omnicron.hsd1.tn.comcast.net>
2014-08-14 22:25:02 -0400

    Local Storage in place

--- Demo/app/js/services.js
+++ Demo/app/js/services.js
@@ -1,77 +1,36 @@
 'use strict';
-
 /* Services */
-angular.module('myApp.services', []).
-        value('version', '0.1')
-        .factory('objectManager',['objectList', function(objectList) {
-             var _objectList1 = objectList;
-             var _objectList2 = [];
-             var _objectList3 = [];
-             var _objectList4 = [];
-             var objectManager = {
-                  getList1: function() {
-                       return _objectList1;
-                  },
-                  addObjectToList1: function(obj) {
-                       var index = _objectList2.indexOf(obj);
-                       if (index === -1)
-                            _objectList1.push(obj);
-                  },
-                  removeObjectFromList1: function(obj) {
-                       var index = _objectList1.indexOf(obj);
+angular.module('myApp.services', [])
+        .value('version', '0.1')
+        .service('objectManager', ['$localStorage', function($localStorage) {
+                  this.List = {List: [
+                            {id: 'OBJ-0', title: 'Drag A Box', name: 'Zero'},
+                            {id: 'OBJ-1', title: 'Drag A Box', name: 'One'},
+                            {id: 'OBJ-2', title: 'Drag A Box', name: 'Two'},
+                            {id: 'OBJ-3', title: 'Drag A Box', name: 'Three'}]};
+
+                  this._master = $localStorage.$default(this.List);
+                  this._nextID = function() {
+                       return 'OBJ-' + this._master.List.length;
+                  };
+                  this.emptyObject = {
+                       id: '',
+                       title: '',
+                       name: ''
+                  };
+                  this.addObject = function(obj) {
+                       obj.id = this._nextID();
+                       this._master.List.push(obj);
+                  };
+                  this.deleteObject = function() {
+                       var index = this._master.List.indexOf(obj);
                        if (index > -1) {
-                            _objectList1.splice(index, 1);
+                            this._master.List.splice(index, 1);
                        }
-                  },
-                  getList2: function() {
-                       return _objectList2;
-                  },
-                  addObjectToList2: function(obj) {
-                       var index = _objectList2.indexOf(obj);
-                       if (index === -1)
-                            _objectList2.push(obj);
-                  },
-                  removeObjectFromList2: function(obj) {
-                       var index = _objectList2.indexOf(obj);
-                       if (index > -1) {
-                            _objectList2.splice(index, 1);
-                       }
-                  },
-                  findObject2: function() {
-                  },
-                  getList3: function() {
-                       return _objectList3;
-                  },
-                  addObjectToList3: function(obj) {
-                       var index = _objectList3.indexOf(obj);
-                       if (index === -1)
-                            _objectList3.push(obj);
-                  },
-                  removeObjectFromList3: function(obj) {
-                       var index = _objectList3.indexOf(obj);
-                       if (index > -1) {
-                            _objectList3.splice(index, 1);
-                       }
-                  },
-                  findObject3: function() {
-                  },
-                  getList4: function() {
-                       return _objectList4;
-                  },
-                  addObjectToList4: function(obj) {
-                       var index = _objectList4.indexOf(obj);
-                       if (index === -1)
-                            _objectList4.push(obj);
-                  },
-                  removeObjectFromList4: function(obj) {
-                       var index = _objectList4.indexOf(obj);
-                       if (index > -1) {
-                            _objectList4.splice(index, 1);
-                       }
-                  },
-                  findObject4: function() {
-                  }
-             };
-             return objectManager;
-        }]);
+                  };
+                  this.getList = function() {
+                       return this._master.List;
+                  };
+             }]);
+
 
